9b711da77cc7a165451f6a5a5946f3984b8252a9
valeri832 <wpgnik@gmail.com>
2024-10-09 17:20:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# "1st program"
+print(9 ** 0.5 * 5)
+# "2nd program"
+print(9.99 > 9.98, 1000 != 1000.1)
+# "3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+# "4th program"
+'123.456'
+print(int(float('123.456') * 10 % 10))
+
+
+
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
